a9869d4da357200fd9e70710405c1cf2dec7e039
CommonNinja <CommonNinja@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-20 07:55:15 +0000

    Adding first version of feeder ninja plugin
    git-svn-id: https://plugins.svn.wordpress.org/feeder-ninja-feed/trunk@775870 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ feederninja-widget.php
@@ -0,0 +1,227 @@
+<?php
+/*
+ * Plugin Name: Feeder Ninja feeds
+ * Plugin URI: http://feederninja.com
+ * Description: Add beautiful <strong>RSS & Social feeds</strong> to your Wordpress website. We're supporting RSS, Facebook, Twitter, YouTube, Vimeo, Thumblr, Pinterest and Wordpress feeds!
+ * Version: 1.0.0
+ * Author: Common Ninja
+ * Author URI: http://commoninja.com/
+ * License: GPLv2 or later
+ */
+
+/**
+ * Register the Widget
+ */
+add_action( 'widgets_init', create_function( '', 'register_widget("feederninja_widget");' ) );
+
+/**
+ * Create the widget class and extend from the WP_Widget
+ */
+ class FeederNinja_Widget extends WP_Widget {
+
+	/**
+	 * Set the widget defaults
+	 */
+	private $widget_title = "Feeder Ninja feeds";
+	private $feed_guid = "7564656dc11643eb8ac657f42afc43c1";
+	private $height = "500";
+
+	private $skip = array();
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	public function __construct() {
+
+		parent::__construct(
+			'FeederNinja_Widget',// Base ID
+			'Feeder Ninja ', // Name
+			array(
+				'classname'		=>	'FeederNinja_Widget',
+				'description'	=>	__('Add a Feeder Ninja feed widget to your website.', 'framework')
+			)
+		);
+
+	} // end constructor
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		extract( $args );
+
+		/* Our variables from the widget settings. */
+		$this->widget_title = apply_filters('widget_title', $instance['title'] );
+
+		$this->show_stream = ($instance['show_stream'] == "1" ?  True : False);
+		$this->feed_guid = $instance['feed_guid'];
+		$this->height = $instance['height'];
+
+		/* Before widget (defined by themes). */
+		echo $before_widget;
+
+		/* Display the widget title if one was input (before and after defined by themes). */
+		if ( $this->widget_title )
+			echo $before_title . $this->widget_title . $after_title;
+
+		/* Render Feeder Ninja */
+        $this->render( array('feed_guid' => $this->feed_guid,
+                             'height' => $this->height) );
+
+		/* After widget (defined by themes). */
+		echo $after_widget;
+	}
+
+    public function render( $args ) {
+        $r = wp_parse_args( $args
+                            , array('feed_guid' => '7564656dc11643eb8ac657f42afc43c1'
+                                    ,'height' => '0'
+                                    ) );
+
+        if ( empty( $r['height'] ) )
+            $r['height'] = '500px';
+        else
+            $r['height'] .= 'px';
+
+        $params = '';
+
+        ?>
+
+        <?php
+        if  ( true ){
+
+        	// accept both a url feed_guid and a regular feed_guid
+        	if( strrpos($r['feed_guid'], "/") !== FALSE ){
+        		$feed_guid_tmp = explode("/", $r['feed_guid']);
+        		$r['feed_guid'] = $feed_guid_tmp[count($feed_guid_tmp) - 1];
+        	}
+
+        	function curPageURL() {
+				$pageURL = 'http';
+				if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+					$pageURL .= "://";
+				if ($_SERVER["SERVER_PORT"] != "80") {
+					$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+				} else {
+					$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+				}
+				return $pageURL;
+			}
+
+			$siteUrl = curPageURL();
+            ?>
+			<div class="feederninja_widget">
+				<iframe style="width: 100%; height: <?php echo $r['height'] ?>; border: none; margin: 0; padding: 0;" src="http://feederninja.com/wordpress/feed/<?php echo $r['feed_guid'] ?>?fnurl=<?php echo $siteUrl ?>" frameborder="0"></iframe>
+			</div>
+            <?php
+        }
+    }
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		
+		/* Strip tags for title and name to remove HTML (important for text inputs). */
+		$instance['feed_guid'] = strip_tags( $new_instance['feed_guid'] );
+		$instance['height'] = strip_tags( $new_instance['height'] );
+
+		return $instance;
+	}
+
+	/**
+	 * Create the form for the Widget admin
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	function form( $instance ) {
+
+		/* Set up some default widget settings. */
+		$defaults = array(
+			'feed_guid' => $this->feed_guid,
+			'height' => $this->height
+		);
+
+		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
+
+        <h4 style="border-bottom: solid 1px #CCC;">Feeder Ninja Settings</h4>
+		
+		<!-- Title: Text Input -->
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'wp_widget_plugin'); ?> </label>
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $instance['title']; ?>" />
+		</p>		
+		
+		<!-- Page name: Text Input -->
+		<p>
+			<label for="<?php echo $this->get_field_id( 'feed_guid' ); ?>"><?php _e('Feed ID', 'framework') ?> </label>
+			<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'feed_guid' ); ?>" name="<?php echo $this->get_field_name( 'feed_guid' ); ?>" value="<?php echo $instance['feed_guid']; ?>" />
+            <br><small style="display: block; text-align: right; margin: 3px 5px 0;"><a href="http://feederninja.com/wordpress" target="_blank">what's this?</a></small>
+		</p>
+
+		<!-- Height: Text Input -->
+		<p>
+			<label for="<?php echo $this->get_field_id( 'height' ); ?>"><?php _e('Height', 'framework') ?> </label>
+			<input type="text" id="<?php echo $this->get_field_id( 'height' ); ?>" name="<?php echo $this->get_field_name( 'height' ); ?>" value="<?php echo $instance['height']; ?>" style="width: 40px" /> px
+		</p>
+
+	<?php
+	}
+ }
+
+
+/**
+ * Display the Feeder Ninja feed
+ *
+ * The list of arguments is below:
+ *     'feed_guid' (string) - Feed guid
+ *     'height' (int) - height of the iframe
+ *
+ * @param string|array $args Optional. Override the defaults.
+ */ 
+function feederninja_embed( $args ) {
+    $rm_widget = new FeederNinja_Widget();
+    $rm_widget->render( $args );
+}
+
+/**
+ * Shortcode to diplay Feeder Ninja in your site.
+ * 
+ * The list of arguments is below:
+ *     'feed_guid' (string) - Feed guid
+ *     'height' (int) - height of the iframe
+ *                    Default: 500
+ */
+function feederninja_shortcode( $atts ) {
+    extract( shortcode_atts( array(
+        'feed_guid' => '7564656dc11643eb8ac657f42afc43c1',
+        'height' => '500'
+    ), $atts ) );
+
+    $rm_widget = new FeederNinja_Widget();
+    
+    $rm_widget->render( array(
+        'feed_guid' => $feed_guid,
+        'height' => $height
+    ) );
+}
+add_shortcode( 'feederninja', 'feederninja_shortcode' );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,88 @@
+=== Feeder Ninja: Create and add RSS & Social feed to your website on-the-fly ===
+Contributors: CommonNinja
+Donate link: http://commoninja.com
+Tags: Facebook widget, Twitter widget, Social Media, Feeds, Feeder Ninja, Ninja, Common Ninja, Instagram widget, Youtube widget, Social Media Feed, Social Feed Generator, import social feeds, Twitter hashtag, display social media, Pinterest widget, integrate Pinterest, RSS widget, RSS Feeds, Custom Feeds, custom social feeds, social feeds plugin, Facebook feed, RSS feed, Twitter feed, Pinterest feed
+Requires at least: 3.0
+Tested up to: 3.6.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+The best tool for adding RSS & Social media feeds to your Wordpress website.
+
+== Description ==
+
+[FeederNinja](http://feederninja.com/about) Feeder Ninja is a useful tool for creating Rss & Social media feeds on the fly. Feeder Ninja using innovative and intuitive user interfaces so you don't need any prior knowledge to use it. We're supporting RSS, Facebook, Twitter, Pinterest, Thumblr, YouTube, Vimeo,and Wordpress [Sing up to Feeder Ninja now](http://feederninja.com) and start creating feeds!
+
+[See thousands of feed examples created with Feeder Ninja.](http://feederninja.com/feeds)
+
+**IMPORTANT: You will FIRST need to go to [Feeder Ninja](http://feederninja.com), sign up for FREE, and create a new feed before you can embed it into your Wordpress website.** 
+
+Got any feedback or question? Contact us via contact{at}feederninja.com.
+
+** It's really easy. Get started in 2 minutes tops, no training needed! **
+
+== Installation ==
+
+= Creating a feed =
+
+1. Create a free account at [Feeder Ninja](http://feederninja.com).
+2. Create a new feed by clicking the "Create new" button on Feeder Ninja's homepage.
+3. Customize your feed.
+4. Click the "Save & Publish" button.
+5. Look for the "Feed ID" in the "Feed Publish" section, copy it, and write it somewhere (we will use it later).
+
+= If you want your Feeder Ninja feed on the Sidebar of your Wordpress website =
+
+1. Download the plugin and upload it into your Wordpress Plugins with the .zip file OR search "Feeder Ninja" and install it.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Go to the Appearance -> Widgets, drag the "Feeder Ninja" widget to your sidebar and change the 'Feed ID' to the feed ID you created on Feeder Ninja's website.
+
+
+= If you want your Feeder Ninja feed as part of one of your Wordpress posts =
+
+1. Create a new Post and click on the HTML option on the top-right hand side of the post.
+2. Copy and paste the following code into your HTML editor: [feederninja feed_guid="FEED_ID"].
+3. Replace the "FEED_ID" with the feed ID you created on Feeder Ninja's website.
+3. Before you publish, click Visibility- Public - Stick this post to the front page.
+
+Arguments provides by the shortcode:
+
+* 'feed_guid' Your feed ID from Feeder Ninja
+* 'height' height of the iframe
+
+== Frequently Asked Questions == 
+ 
+= Q: What is Feeder Ninja? =
+
+Feeder Ninja is a useful tool for web designers, developers and site owners to create Rss & Social feeds on the fly.
+
+= Q: What can I use Feeder Ninja for? =
+
+You can use Feeder Ninja to create and add beautiful RSS & Social media feeds to your Wordpress website. We're supporting Facebook, Twitter, Pinterest, Thumblr, YouTube, Vimeo,and Wordpress.
+
+= Q: Who can use Feeder Ninja =
+
+Everyone. Really, it's that simple. We're using innovative and intuitive user interfaces so you don't need any prior knowledge to use it.
+
+= Q: How do I start using Feeder Ninja? =
+
+Sign up to [Feeder Ninja](http://feederninja.com), create a new feed and install the Feeder Ninja plugin on your Wordpress admin panel.
+
+
+== Screenshots ==
+
+1. Feeder Ninja on Sidebar
+2. Example of Feeder Ninja as a Sidebar
+3. Feeder Ninja in a post
+4. Example of Feeder Ninja as a part of a post
+
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial Version
+
+== Upgrade Notice ==
+
+No problems should be encountered if upgrading.
